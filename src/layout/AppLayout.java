6a6988f34e812fd219915befee8182f643362a17
Lucas <lucasatletico@hotmail.com>
2020-06-01 17:36:24 -0300

    Gerenciador de layout usando border pane

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -1,17 +1,20 @@
 package layout;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		Parent raiz = null;
 		
+		//raiz = new TesteAnchorPane();
+		raiz = new TesteBorderPane();
 		
-		Scene principal = new Scene(new TesteAncorPane(), 800, 600);
+		Scene principal = new Scene(raiz, 800, 600);
 		
 		primaryStage.setScene(principal);
 		primaryStage.setTitle("Gerenciadores de layout");
